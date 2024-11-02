8947da09fe67633101b83c4c3a4c27c06d0c57bc
tubatoycorn <batcakesteak@protonmail.com>
2024-11-02 17:31:46 +0100

    Optimized craco config

--- craco.config.js
+++ craco.config.js
@@ -4,77 +4,89 @@ const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [
-        require("autoprefixer"),
-        purgecss({
-          content: [
-            "./src/**/*.html",
-            "./src/**/*.jsx",
-            "./src/**/*.js",
-            "./src/**/*.tsx",
-            "./src/**/*.ts",
-          ],
-          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        }),
-      ],
-    },
-  },
-  webpack: {
-    plugins: [
-      new CompressionPlugin({
-        test: /\.(js|css|html|svg)$/,
-        threshold: 10240,
-        minRatio: 0.8,
-        algorithm: "brotliCompress",
-      }),
-      new BundleAnalyzerPlugin({
-        analyzerMode: "static",
-        openAnalyzer: false,
-        reportFilename: "bundle-report.html",
-      }),
-    ],
-    configure: (webpackConfig) => {
-      webpackConfig.output.filename = "static/js/[name].[contenthash:8].js";
-      webpackConfig.output.chunkFilename =
-        "static/js/[name].[contenthash:8].chunk.js";
-
-      webpackConfig.optimization = {
-        splitChunks: {
-          chunks: "all",
-          maxInitialRequests: 10,
-          maxAsyncRequests: 20,
-          cacheGroups: {
-            default: false,
-            vendors: false,
-            vendor: {
-              name: "vendor",
-              chunks: "all",
-              test: /[\\/]node_modules[\\/]/,
-              priority: 20,
-            },
-            common: {
-              name: "common",
-              chunks: "all",
-              minChunks: 2,
-              priority: 10,
-              reuseExistingChunk: true,
-              enforce: true,
-            },
-          },
+    style: {
+        postcss: {
+            plugins: [
+                require("autoprefixer"),
+                purgecss({
+                    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+                    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+                }),
+            ],
         },
-        runtimeChunk: "single",
-        minimizer: [new TerserPlugin()],
-      };
-
-      return webpackConfig;
     },
-  },
-  devServer: {
-    compress: true,
-    port: 3000,
-    historyApiFallback: true,
-    hot: true,
-  },
+    webpack: {
+        plugins: [
+            new CompressionPlugin({
+                test: /\.(js|css|html|svg)$/,
+                threshold: 8192,
+                minRatio: 0.8,
+                algorithm: "brotliCompress",
+            }),
+            process.env.ANALYZE &&
+                new BundleAnalyzerPlugin({
+                    analyzerMode: "static",
+                    openAnalyzer: false,
+                    reportFilename: "bundle-report.html",
+                }),
+        ].filter(Boolean),
+
+        configure: (webpackConfig) => ({
+            ...webpackConfig,
+            output: {
+                ...webpackConfig.output,
+                filename: "static/js/[name].[contenthash:8].js",
+                chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
+            },
+            optimization: {
+                ...webpackConfig.optimization,
+                splitChunks: {
+                    chunks: "all",
+                    maxInitialRequests: 10,
+                    maxAsyncRequests: 20,
+                    cacheGroups: {
+                        vendor: {
+                            name: "vendor",
+                            test: /[\\/]node_modules[\\/]/,
+                            chunks: "all",
+                            priority: 20,
+                        },
+                        common: {
+                            name: "common",
+                            minChunks: 2,
+                            chunks: "all",
+                            priority: 10,
+                            reuseExistingChunk: true,
+                            enforce: true,
+                        },
+                    },
+                },
+                runtimeChunk: "single",
+                minimizer: [
+                    new TerserPlugin({
+                        terserOptions: {
+                            compress: {
+                                ecma: 5,
+                                warnings: false,
+                                comparisons: false,
+                                inline: 2,
+                            },
+                            mangle: { safari10: true },
+                            output: {
+                                ecma: 5,
+                                comments: false,
+                                ascii_only: true,
+                            },
+                        },
+                    }),
+                ],
+            },
+        }),
+    },
+    devServer: {
+        compress: true,
+        port: 3000,
+        historyApiFallback: true,
+        hot: true,
+    },
 };
