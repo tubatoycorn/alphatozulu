a128908b7f9f7027d5d454bf48948c88be15ab97
tubatoycorn <batcakesteak@protonmail.com>
2024-11-02 17:51:40 +0100

    Improved Home component structure

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,26 +2,26 @@ import { useContext, lazy, Suspense } from "react";
 import { useAlphabetConverter } from "../../hooks/useAlphabetConverter";
 import { AlphabetContext } from "../../contexts/AlphabetContext";
 
-const AlphabetSelector = lazy(() =>
-  import("../../components/AlphabetSelector/AlphabetSelector")
-);
+const AlphabetSelector = lazy(() => import("../../components/AlphabetSelector/AlphabetSelector"));
 const TextInput = lazy(() => import("../../components/TextInput/TextInput"));
 
-const Home = () => {
-  const { alphabet } = useContext(AlphabetContext);
-  const { convertText } = useAlphabetConverter(alphabet);
+const LoadingFallback = () => <div className="text-center p-3">Loading...</div>;
 
-  return (
-    <div className="container mt-5">
-      <header className="text-center mb-4">
-        <h1>AlphaToZulu: Phonetic Alphabet Translator</h1>
-      </header>
-      <Suspense fallback={<div>Loading...</div>}>
-        <AlphabetSelector />
-        <TextInput convertText={convertText} />
-      </Suspense>
-    </div>
-  );
+const Home = () => {
+    const { alphabet } = useContext(AlphabetContext);
+    const { convertText } = useAlphabetConverter(alphabet);
+
+    return (
+        <div className="container mt-5">
+            <header className="text-center mb-4">
+                <h1>AlphaToZulu: Phonetic Alphabet Translator</h1>
+            </header>
+            <Suspense fallback={<LoadingFallback />}>
+                <AlphabetSelector />
+                <TextInput convertText={convertText} />
+            </Suspense>
+        </div>
+    );
 };
 
 export default Home;
